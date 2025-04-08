3af31d0289ef891b91f40b6b7f75616c1396da05
saloni jain <jainsaloni498@gmail.com>
2025-04-08 12:04:29 +0530

    SOLID Principles

--- taxi_service/rides/admin.py
+++ taxi_service/rides/admin.py
@@ -1,16 +1,39 @@
+# from django.contrib import admin
+# from .models import Driver, Ride
+
+# @admin.register(Driver)
+# class DriverAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'latitude', 'longitude', 'is_available', 'rating', 'car_type')
+#     list_filter = ('is_available', 'car_type')
+#     search_fields = ('user__username', 'car_type')
+
+# @admin.register(Ride)
+# class RideAdmin(admin.ModelAdmin):
+#     list_display = ('customer', 'driver', 'pickup_latitude', 'pickup_longitude', 'drop_latitude', 'drop_longitude', 'status', 'fare')
+#     list_filter = ('status', 'car_type')
+#     search_fields = ('customer__username', 'driver__user__username')
+#     readonly_fields = ('cancellation_fee', 'cancellation_reason')
+
 from django.contrib import admin
 from .models import Driver, Ride
 
-@admin.register(Driver)
-class DriverAdmin(admin.ModelAdmin):
-    list_display = ('user', 'latitude', 'longitude', 'is_available', 'rating', 'car_type')
+class DriverFilterMixin:
     list_filter = ('is_available', 'car_type')
+
+class DriverSearchMixin:
     search_fields = ('user__username', 'car_type')
 
-@admin.register(Ride)
-class RideAdmin(admin.ModelAdmin):
-    list_display = ('customer', 'driver', 'pickup_latitude', 'pickup_longitude', 'drop_latitude', 'drop_longitude', 'status', 'fare')
+class RideFilterMixin:
     list_filter = ('status', 'car_type')
-    search_fields = ('customer__username', 'driver__user__username')
-    readonly_fields = ('cancellation_fee', 'cancellation_reason')
 
+class RideSearchMixin:
+    search_fields = ('customer__username', 'driver__user__username')
+
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin, DriverFilterMixin, DriverSearchMixin):
+    list_display = ('user', 'latitude', 'longitude', 'is_available', 'rating', 'car_type')
+
+@admin.register(Ride)
+class RideAdmin(admin.ModelAdmin, RideFilterMixin, RideSearchMixin):
+    list_display = ('customer', 'driver', 'pickup_latitude', 'pickup_longitude', 'drop_latitude', 'drop_longitude', 'status', 'fare')
+    readonly_fields = ('cancellation_fee', 'cancellation_reason')
--- taxi_service/rides/utils.py
+++ taxi_service/rides/utils.py
@@ -1,22 +1,58 @@
+# import math
+# import requests
+# from geopy.distance import geodesic
+
+# def calculate_approx_distance(lat1, lon1, lat2, lon2):
+#     lat_avg = (lat1 + lat2) / 2
+#     lat_dist = 111.32 * (lat2 - lat1)
+#     lon_dist = (40075 * math.cos(math.radians(lat_avg)) / 360) * (lon2 - lon1)
+#     return math.sqrt(lat_dist ** 2 + lon_dist ** 2)
+
+# def find_nearest_driver(pickup_lat, pickup_lng):
+#     from rides.models import Driver 
+
+#     available_drivers = Driver.objects.filter(is_available=True)
+#     nearest_driver = None
+#     min_distance = float('inf')
+
+#     for driver in available_drivers:
+#         distance = geodesic((pickup_lat, pickup_lng), (driver.latitude, driver.longitude)).km
+#         if distance < min_distance:
+#             min_distance = distance
+#             nearest_driver = driver
+
+#     return nearest_driver
+
+# def get_travel_time(pickup_lat, pickup_lon, drop_lat, drop_lon):
+#     pass  
+
 import math
 import requests
 from geopy.distance import geodesic
+from rides.models import Driver 
 
-def calculate_approx_distance(lat1, lon1, lat2, lon2):
-    lat_avg = (lat1 + lat2) / 2
-    lat_dist = 111.32 * (lat2 - lat1)
-    lon_dist = (40075 * math.cos(math.radians(lat_avg)) / 360) * (lon2 - lon1)
-    return math.sqrt(lat_dist ** 2 + lon_dist ** 2)
+class DistanceCalculator:    
+    @staticmethod
+    def geodesic_distance(lat1, lon1, lat2, lon2):
+        return geodesic((lat1, lon1), (lat2, lon2)).km
 
-def find_nearest_driver(pickup_lat, pickup_lng):
-    from rides.models import Driver 
+    @staticmethod
+    def approximate_distance(lat1, lon1, lat2, lon2):
+        lat_avg = (lat1 + lat2) / 2
+        lat_dist = 111.32 * (lat2 - lat1)
+        lon_dist = (40075 * math.cos(math.radians(lat_avg)) / 360) * (lon2 - lon1)
+        return math.sqrt(lat_dist ** 2 + lon_dist ** 2)
 
-    available_drivers = Driver.objects.filter(is_available=True)
+def get_available_drivers():
+    return Driver.objects.filter(is_available=True)
+
+def find_nearest_driver(pickup_lat, pickup_lng, distance_calculator=DistanceCalculator()):
+    available_drivers = get_available_drivers()
     nearest_driver = None
     min_distance = float('inf')
 
     for driver in available_drivers:
-        distance = geodesic((pickup_lat, pickup_lng), (driver.latitude, driver.longitude)).km
+        distance = distance_calculator.geodesic_distance(pickup_lat, pickup_lng, driver.latitude, driver.longitude)
         if distance < min_distance:
             min_distance = distance
             nearest_driver = driver
@@ -24,4 +60,4 @@ def find_nearest_driver(pickup_lat, pickup_lng):
     return nearest_driver
 
 def get_travel_time(pickup_lat, pickup_lon, drop_lat, drop_lon):
-    pass  
+    pass
